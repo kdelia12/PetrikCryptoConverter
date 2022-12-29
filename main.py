35fc091a269c9bd5b8771984891082618b1d77d3
kdelia12 <wild12748@gmail.com>
2022-12-30 03:44:21 +0700

    fixing start

--- main.py
+++ main.py
@@ -17,27 +17,18 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message: telebot.types.Message):
     text = "Convert a currency into another.\n" \
-           "Example: bitcoin dollar 2.5 - \n" \
-           "shows the price of 2.5 bitcoins in US dollars.\n" \
+           "Example: /convert 1 btc idr - \n" \
+           "shows the price of 1 bitcoins in US dollars.\n" \
            "Use /help for other options."
     bot.reply_to(message, text)
 
 @bot.message_handler(commands=['help'])
 def help(message: telebot.types.Message):
     text = "/help - shows this message.\n" \
-           "/curr - gives a list of supported currencies.\n" \
-           "<cur1> <cur2> <quantity> - shows the price\n" \
-           "of <quantity> units of <cur1> in <cur2>.\n" \
-           "Example: bitcoin dollar 2.5 - shows the price\n" \
-           "of 2.5 bitcoin in US dollars\n"
+           "Example: /convert 1 btc idr - shows the price\n" \
+           "of 1 bitcoin in US dollars\n"
     bot.reply_to(message, text)
 
-@bot.message_handler(commands=['curr'])
-def list_of(message: telebot.types.Message):
-    text = 'Available currencies:'
-    for key in keys.keys():
-        text = '\n'.join((text, key, ))
-    bot.reply_to(message, text)
 
 @bot.message_handler(commands=['convert'])
 def convert(message: telebot.types.Message):
