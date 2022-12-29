74848eb971abe61ea1f19594ddd0a8ce463d5164
kdelia12 <wild12748@gmail.com>
2022-12-30 05:23:26 +0700

    remove deprecated function

--- config.py
+++ config.py
@@ -5,24 +5,4 @@ Configuration file for Telegram bot
 # Telegram bot token
 TOKEN = ''
 
-# The list of supported currencies.
-# From cryptocompare.com API.
-# Can be easily extended.
-keys = {
-    'btc'   : 'BTC',
-    'eth'  : 'ETH',
-    'tether'    : 'USDT',
-    'bnb'       : 'BNB',
-    'ripple'    : 'XRP',
-    'polygon'   : 'MATIC',
-    'polkadot'  : 'DOT',
-    'litecoin'  : 'LTC',
-    'solana'    : 'SOL',
-    'uniswap'   : 'UNI',
-    'avalanche' : 'AVAX',
-    'chainlink' : 'LINK',
-    'usd'    : 'USD',
-    'idr' : 'IDR',
-}
-
 # End of file
